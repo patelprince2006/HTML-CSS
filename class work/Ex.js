62d02c9b41af87b6676cd1106ced625dd35f76ef
prince <patelprince23906@gmail.com>
2025-07-18 23:43:32 +0530

    initial commit

--- class work/Ex.js
+++ class work/Ex.js
@@ -1,4 +1,31 @@
 // alert("Hello");
-var msg;
-msg="hello12";
-alert(msg);
+
+     //let msg;
+    //msg="hello12";
+    //alert(msg);
+        // var name;
+        // var admin;
+        // name="prince";
+        // alert("wll come"+" "+name);
+        // alert(name);
+
+// var msg;         
+// msg="hello12";
+// alert(msg);
+
+//if stetment
+
+
+if(true){
+    let str="inside if "
+    const fname="hello";
+    console.log(str);
+}
+
+//click button to chNGE
+function myfunction(){
+    document.getElementById("demo").innerText="paragraph chnage"
+}
+
+console.log(str);
+console.log(fname);
